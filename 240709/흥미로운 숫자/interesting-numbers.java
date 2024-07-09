f402349cfc3c73c08c7ec74f73e6f6e1e029df46
Hyeon0706 <83438780+Hyeon0706@users.noreply.github.com>
2024-07-09 14:48:04 +0900

    Title: 흥미로운 숫자, Time: 131ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240709/흥미로운 숫자/interesting-numbers.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int x, y;
+    static int ans;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        HashSet<Character> set;
+        for(int num=x; num<=y; num++){
+            set = new HashSet<>();
+            char[] number = String.valueOf(num).toCharArray();
+            for(char c : number){
+                set.add(c);
+            }
+            if(set.size()==2){
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
